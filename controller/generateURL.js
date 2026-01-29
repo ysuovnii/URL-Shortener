2c6ce8397117641290c4b04d78321ed4a5cb813d
ysuovnii <ysuovnii@gmail.com>
2026-01-29 23:45:05 +0530

    better error handling

--- controller/generateURL.js
+++ controller/generateURL.js
@@ -2,19 +2,29 @@ const urlSchema = require('../model/urlSchema')
 const {nanoid} = require('nanoid')
 
 async function generateURL(req, res) {
-    url = req.body.url 
-    if(!url) {
-        return res.status(400).json({"error" : "URL is required"})
+    try {
+
+        url = req.body.url 
+        if(!url) {
+            return res.status(400).json({"error" : "URL is required"})
+        }
+        
+        id = nanoid(8)
+        
+        await urlSchema.create({
+            shortID : id,
+            redirectURL : url, 
+        })
+        
+        return res.json({
+            "shortID" : id,
+            "RedirectURL" : url,
+        })
     }
-
-    id = nanoid(8)
-
-    await urlSchema.create({
-        shortID : id,
-        redirectURL : url, 
-    })
-
-    return res.json({"shortID" : id})
+    catch(err) {
+        console.log("error: ", err)
+        return res.status(500).json({"error" : "Something went wrong"})
+    }
 }
 
 module.exports = {generateURL}
